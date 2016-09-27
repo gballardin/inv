907a5c05a9650635cf8f0e0e5b79a669f3f504ad
Giorgio Ballardin <gballardin@ebay.com>
2016-09-26 21:20:13 -0700

    Working version with Fidelity ETFs example.

--- 01-efficient_frontier.R
+++ 01-efficient_frontier.R
@@ -3,46 +3,50 @@ library(stockPortfolio) # Base package for retrieving returns
 library(ggplot2) # Used to graph efficient frontier
 library(reshape2) # Used to melt the data
 library(quadprog) #Needed for solve.QP
+library(magrittr)
 library(readxl)
+library(quantmod)
+library(tidyr)
 
 #source functions
 setwd("~/Documents/funds")
 source('00-utilities.R')
 
 #get ticker data
-etf_base <- read_excel('Screener-Export Mutual Funds.xls', sheet = 'Current Criteria')
+# etf_base <- read_excel('Screener-Export Mutual Funds.xls', sheet = 'Current Criteria')
+etf_base <- read_excel('Screener-Export ETF.xls', sheet = 'Current Criteria')
+etf_base$`ETF Name` <- tolower(etf_base$`ETF Name`)
+etf_base <- etf_base[grepl(pattern = '.*fidelity.*', x = etf_base$`ETF Name`), ]
+
 
 #pull data from the Internet
-tickers <- etf_base$Symbol[1:1e2]
+tickers <- etf_base$Ticker
 Stocks <- lapply(tickers, function(sym) {
-  weeklyReturn(na.omit(getSymbols(sym, auto.assign=FALSE)))
-  
+  xts_out <- dailyReturn(na.omit(getSymbols(sym, auto.assign=FALSE)))
+  colnames(xts_out) <- sym
+  xts_out
 })
+
 df <- do.call(merge.xts,Stocks)
-colnames(df) <- tickers
 df <- cbind(date = index(df), as.data.frame(df))
-date_threshold <- sapply(df, function(x) sum(is.na(x))) %>% quantile(prob = .1)
-df <- df[1:(date_threshold+1), ]
+date_threshold <- sapply(df, function(x) sum(is.na(x))) %>% quantile(prob = .9)
+keep_ticker <- sapply(df, function(x) sum(is.na(x))) < date_threshold
+df <- df[df$date>as.Date('2013-10-24'), keep_ticker]
+df <- df[,sapply(df, function(x) (sum(is.na(x)) / length(x)) < .1)]
+# nrow(df)
+# sum(complete.cases(df))
+
 returns <- as.matrix(df[complete.cases(df),-1])
 
-#remove stocks with short lifespan
-ftr_df <- group_by(df, ticker) %>% filter(n()>2000)
-ftr_df <- droplevels(ftr_df)
-table(table(ftr_df$ticker))
-
 #Plot returns
-group_by(ftr_df, ticker) %>%
-  #filter(ticker %in% stocks) %>%
-  mutate(return = log(close) - lag(log(close))) %>%
-  summarise(mean_ret = mean(return, na.rm = TRUE), sd_ret = sd(return, na.rm = TRUE)) %>% 
-  ggplot(aes(x=sd_ret, y=mean_ret, label = ticker)) + geom_point()
-
-+ geom_text(check_overlap = TRUE)
-
-
+ftr_df <- tidyr::gather(df, key = ticker, value = value, 2:ncol(df))
+dplyr::group_by(ftr_df, ticker) %>%
+  dplyr::summarise(mean_ret = mean(value, na.rm = TRUE), sd_ret = sd(value, na.rm = TRUE)) %>% 
+  ggplot(aes(x=sd_ret, y=mean_ret, label = ticker)) + geom_point() +
+  geom_text(check_overlap = TRUE)
 
 # Run the eff.frontier function based on no short and 50% alloc. restrictions
-eff <- eff.frontier(returns=returns, short="no", max.allocation=.2,
+eff <- eff.frontier(returns=returns, short="no", max.allocation=.5,
                     risk.premium.up=1, risk.increment=.001)
 
 # Find the optimal portfolio
